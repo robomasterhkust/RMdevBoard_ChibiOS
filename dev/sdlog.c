bb8026c0f0aa7039e961ae2eb5169c0fb4f6739a
nameless1379 <yzhangdk@ust.hk>
2018-01-22 15:50:59 +0800

    prototype SD trajectory recorder

--- dev/sdlog.c
+++ dev/sdlog.c
@@ -4,6 +4,8 @@
 
 #include "ff.h"
 
+sdlogStruct logger;
+
 /*
  * Working area for driver.
  */
@@ -19,6 +21,20 @@ static const SDCConfig sdccfg = {
   SDC_MODE_4BIT
 };
 
+/**
+ *   @brief   return the error code of SD log
+ *   @api
+ */
+uint32_t sdlog_getError(void)
+{
+  return (uint32_t)logger.errorCode;
+}
+
+/**
+ *   @brief   create a new file on SD card
+ *   @NOTE    will overwrite file with same name
+ *   @notapi
+ */
 uint8_t sdlog_createFile(char fileName[])
 {
   FRESULT fr;
@@ -43,10 +59,57 @@ uint8_t sdlog_createFile(char fileName[])
   return (uint8_t)fr;
 }
 
-static THD_WORKING_AREA(sdlog_thread_wa,256);
+/**
+ *    @brief  indicate the data to be logged
+ *    @param  pos: position of the data to be logged [0-32]
+ *    @param  buf: data array to be logged
+ *    @param  len: length of data array in byte
+ *    @NOTE   return -1 if that paticular position is occupied
+ *    @api
+ */
+uint8_t sdlog_put(const uint8_t pos, const void* const buf, const uint8_t len)
+{
+  //TODO put the buf* and length to sdlogstruct and verify it
+  return 0;
+}
+
+/**
+ *  @brief  write log file with the defined format
+ *  @return the error code while writing, 0 if suceeded
+ *  @notapi
+ */
+static inline uint8_t sdlog_write(const uint8_t pos)
+{
+  uint8_t len;
+  FRESULT fr;
+  fr = f_write(&fil, logger.buf[pos], logger.len_buf[pos], &len);
+
+  return (uint8_t)fr;
+}
+
+static THD_WORKING_AREA(sdlog_thread_wa,1024);
 static THD_FUNCTION(sdlog_thread,p)
 {
   uint32_t tick = chVTGetSystemTimeX();
+
+  /* for test*/
+  char buf1[] = "   \n";
+  char buf2[] = "12345678\n12345678\n";
+  char buf3[] = "abcdefgh\n12345678\n";
+  char buf4[] = "abcdefgh\nabcdefgh\n";
+  logger.buf[0] = buf1;
+  logger.buf[1] = buf2;
+  logger.buf[2] = buf3;
+  logger.buf[3] = buf4;
+  logger.len_buf[0] = 4;
+  logger.len_buf[1] = 18;
+  logger.len_buf[2] = 18;
+  logger.len_buf[3] = 18;
+  logger.position = 0x0006;
+  /**/
+
+  f_open(&fil, "test.txt",  FA_WRITE);
+  f_lseek(&fil, 0);
 
   while(true)
   {
@@ -56,26 +119,50 @@ static THD_FUNCTION(sdlog_thread,p)
     else
     {
       tick = chVTGetSystemTimeX();
+      logger.errorCode |= SD_LOSE_FRAME;
     }
+
+    uint8_t i, error;
+    for(i = 0; i<SDLOG_NUM_BUFFER; i++)
+    {
+      if(!(logger.position & (1<<i)))
+        continue;
+      error = sdlog_write(i);
+      if(error)
+      {
+        logger.errorCode |= (error << 7);
+        break;
+      }
+    }
+
+    if(!error)
+      f_sync(&fil);
   }
 }
 
+/**
+ *   @brief   initializes SD logger
+ *   @api
+ */
 void sdlog_init(void)
 {
+  memset((void*)&logger, 0, sizeof(sdlogStruct));
   sdcStart(&SDCD1,&sdccfg);
 
   /* Card presence check.*/
   if (!blkIsInserted(&SDCD1))
   {
+    logger.errorCode |= SD_NOCARD;
     return;
   }
 
   if (sdcConnect(&SDCD1))
   {
+    logger.errorCode |= SD_NOCONNECT;
     return;
   }
 
-  uint8_t result = sdlog_createFile("test.txt");
+  logger.errorCode |= sdlog_createFile("test.txt");
 
   chThdCreateStatic(sdlog_thread_wa, sizeof(sdlog_thread_wa),
                     NORMALPRIO - 10,
