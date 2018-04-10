bc6b453106ab3ec473de76ef722e7f82b9cd0fc0
Beck <pshfls@gmail.com>
2018-04-10 16:39:19 +0800

    remove the MAVLink, switch to CAN protocal

--- 5_task/task.mk
+++ 5_task/task.mk
@@ -10,10 +10,6 @@ TASKSRC =   \
             $(TASK_PATH)/command_mixer_task.c
             # $(TASK_PATH)/canBusProcess.c \
             # $(TASK_PATH)/error.c \
-            # $(TASK_PATH)/mavlink_comm.c \
-            # $(TASK_PATH)/mavlink_topic.c
 
 # Required include directories
-TASKINC =   $(TASK_PATH)/inc \
-            $(TASK_PATH)/mavlink \
-            $(TASK_PATH)/mavlink/minimal
+TASKINC =   $(TASK_PATH)/inc 
