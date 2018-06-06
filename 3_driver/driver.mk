4130c352558a1249e3f9db997a612b8ddd55d107
Alex Wong Tat Hang <32214395+TopQuark12@users.noreply.github.com>
2018-06-06 08:54:46 +0800

    Rewrote adis16470 driver, stable and working
    Features:
    gyro data for 3 axis
    acc data for 3 axis
    temperature data
    calibrate bias for gyro and acc
    data can be saved to imu flash

--- 3_driver/driver.mk
+++ 3_driver/driver.mk
@@ -5,11 +5,11 @@ DRIVERSRC = \
             $(DRIVER_PATH)/judge.c     \
             $(DRIVER_PATH)/adis16265.c \
             $(DRIVER_PATH)/uwb_driver.c \
-            $(DRIVER_PATH)/mpu6500.c
+            $(DRIVER_PATH)/mpu6500.c \
+            $(DRIVER_PATH)/adis16470.c 
             # $(DRIVER_PATH)/hcsr04.c  \
             # $(DRIVER_PATH)/ist8310.c \
-            # $(DRIVER_PATH)/tft_display.c \
-            # $(DRIVER_PATH)/adis16470.c \
+            # $(DRIVER_PATH)/tft_display.c \            
 
 # Required include directories
 DRIVERINC = $(DRIVER_PATH)/inc
