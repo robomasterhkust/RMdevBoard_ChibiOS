233bfa549339df163b470a1e8041126f032efc6d
zengkuang <kzeng@connect.ust.hk>
2018-06-30 20:23:28 +0800

    Magazine handle

--- dev/inc/magazine_cover_task.h
+++ dev/inc/magazine_cover_task.h
@@ -1,7 +1,8 @@
 #ifndef _MAGAZINE_COVER_TASK_H_
 #define _MAGAZINE_COVER_TASK_H_
 
-
+#define Maga_UPDATE_FREQ 50
+#define Maga_UPDATE_PERIOD_US 1000000/Maga_UPDATE_FREQ
 void pwm_magazine_cover_init(void);
 
 #endif
